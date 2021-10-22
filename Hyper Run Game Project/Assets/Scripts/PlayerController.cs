c1026fd824d3cec8742b1343c4207dbd1cd0aeb6
yashed <86907536+yashed@users.noreply.github.com>
2021-10-22 21:49:21 +0530

    Adding Comments to the Player Controller Script
    Add Comments

--- Hyper Run Game Project/Assets/Scripts/PlayerController.cs
+++ Hyper Run Game Project/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables 
     public Rigidbody Rb;
     public Animator anim;
 
+    //Float variables to player controll
     public float speed;
     public float JumpSpeed;
     float VarVal;
 
+    //Bool variables for the player animation management
     bool Jump;
     bool Run;
     bool Climb;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +30,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Assing Verical value to the VarVal
         VarVal = Input.GetAxisRaw("Vertical");
 
+
+        //Player Run
         if (VarVal > 0.1 && Climb == false && Run == true)
         {
             Debug.Log("VarVal 1");
@@ -34,27 +42,32 @@ public class PlayerController : MonoBehaviour
 
         }
 
+        //Player Climb
         else if (VarVal > 0.1 && Climb == true)
         {
             transform.Translate(0, speed * Time.deltaTime, 0);
         }
 
+        //Player Jump
         else if (VarVal > 0.1 && Jump == true)
         {
             Debug.Log("jemp speed");
             transform.position += Vector3.forward * Time.deltaTime * JumpSpeed;
         }
 
+
+        //Set Animator Parameters to the variables
         anim.SetFloat("Run", VarVal);
         anim.SetBool("jump", Jump);
         anim.SetBool("climb", Climb);
     }
 
+
     private void OnTriggerEnter(Collider Coll)
     {
+
         if (Coll.tag == "Jump")
         {
-       
             Debug.Log("jump");
             Jump = true;
             Run = false;
@@ -63,7 +76,6 @@ public class PlayerController : MonoBehaviour
 
         if (Coll.tag == "Ground")
         {
-            
             Debug.Log("jump false");
             Jump = false;
             Run = true;
