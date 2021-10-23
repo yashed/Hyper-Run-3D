3441b35419dbcdf640fab119786516dc5e1bf67a
yashed <86907536+yashed@users.noreply.github.com>
2021-10-23 21:33:42 +0530

    Player Fall

---
+++ Hyper Run Game Project/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    bool Fall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //Player Management (Respawn / Dead / Fall)
+    private void OnTriggerEnter(Collider Coll)
+    {
+        if (Coll.tag == "Hummer")
+        {
+            Debug.Log("Hummer Collied");
+            Fall = true;
+        }
+    }
+}
