ffc5de3183f13498dcd48a6f83a9e115bde0a6a7
yashed <86907536+yashed@users.noreply.github.com>
2021-10-23 23:19:36 +0530

    Respawn

--- Hyper Run Game Project/Assets/Scripts/PlayerHealthManager.cs
+++ Hyper Run Game Project/Assets/Scripts/PlayerHealthManager.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
 {
+    public Animator anim;
+
+    public GameObject Player;
+    public Transform SpawnPos;
+
+
     bool Fall;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("Fall", Fall);
     }
 
+    void SpawnPlayer()
+    {
+        Instantiate(Player,SpawnPos.position ,Quaternion.identity );
+    
+    }
 
     //Player Management (Respawn / Dead / Fall)
     private void OnTriggerEnter(Collider Coll)
@@ -26,6 +38,15 @@ public class PlayerHealthManager : MonoBehaviour
         {
             Debug.Log("Hummer Collied");
             Fall = true;
+        }
+
+        if (Coll.tag == "respawn")
+        {
+            Fall = false;
+            Destroy(gameObject);
+            SpawnPlayer();
+            
+
         }
     }
 }
