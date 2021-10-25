6b2b7e3da604e7d924c16493adf73b1e877db6bf
yashed <86907536+yashed@users.noreply.github.com>
2021-10-25 12:40:50 +0530

    obstacles manage

---
+++ Hyper Run Game Project/Assets/Scripts/Obstacles1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles1 : MonoBehaviour
+{
+    public float obsSpeed;
+    public bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveRight)
+        {
+            transform.Translate(obsSpeed * Time.deltaTime, 0, 0);
+
+        }
+
+        else
+        {
+            transform.Translate(-(obsSpeed * Time.deltaTime), 0, 0);
+        }
+        
+    }
+
+      private void OnTriggerEnter(Collider Coll)
+    {
+        if (Coll.tag == "turn")
+        {
+            Debug.Log("Collied");
+            if (moveRight)
+            {
+                moveRight = false;
+            }
+            else
+            {
+                moveRight = true;
+            }
+        }
+    }
+}
